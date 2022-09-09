eb66d29e2426c63fcb89f418f7662e9de1e92b7c
Jason Kartz <jasonkartz@gmail.com>
2022-09-09 18:41:47 -0500

    home banner - mobile

--- pages/index.js
+++ pages/index.js
@@ -2,29 +2,28 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>H1 Heading</h1>
-      <h2>H2 Heading</h2>
-      <h3>H3 Heading</h3>
-      <button className="btn-on-dark">learn more</button>
-      Our Company Locations Contact Award-winning custom designs and digital
-      branding solutions With over 10 years in the industry, we are experienced
-      in creating fully responsive websites, app design, and engaging brand
-      experiences. Find out more about our services. Learn more Web Design View
-      Projects App Design View Projects Graphic Design View Projects Passionate
-      Each project starts with an in-depth brand research to ensure we only
-      create products that serve a purpose. We merge art, design, and technology
-      into exciting new solutions. Resourceful Everything that we do has a
-      strategic purpose. We use an agile approach in all of our projects and
-      value customer collaboration. It guarantees superior results that fulfill
-      our clients’ needs. Friendly We are a group of enthusiastic folks who know
-      how to put people first. Our success depends on our customers, and we
-      strive to give them the best experience a company can provide. Let’s talk
-      about your project Ready to take it to the next level? Contact us today
-      and find out how our expertise can help your business grow. Get in touch
-      Our company Locations Contact Designo Central Office 3886 Wellington
-      Street Toronto, Ontario M9C 3J5 Contact Us (Central Office) P : +1
-      253-863-8967 M : contact@designo.co
-    </div>
+    <>
+      <section className="banner-home">
+        <h1>Award-winning custom designs and digital branding solutions </h1>
+        <p>
+          With over 10 years in the industry, we are experienced in creating
+          fully responsive websites, app design, and engaging brand experiences.
+          Find out more about our services.
+        </p>
+        <button className="btn-on-dark">Learn more </button>
+      </section>
+      Web Design View Projects App Design View Projects Graphic Design View
+      Projects Passionate Each project starts with an in-depth brand research to
+      ensure we only create products that serve a purpose. We merge art, design,
+      and technology into exciting new solutions. Resourceful Everything that we
+      do has a strategic purpose. We use an agile approach in all of our
+      projects and value customer collaboration. It guarantees superior results
+      that fulfill our clients’ needs. Friendly We are a group of enthusiastic
+      folks who know how to put people first. Our success depends on our
+      customers, and we strive to give them the best experience a company can
+      provide. Let’s talk about your project Ready to take it to the next level?
+      Contact us today and find out how our expertise can help your business
+      grow. Get in touch
+    </>
   );
 }

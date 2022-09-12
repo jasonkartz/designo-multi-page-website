ed1b993baafc0005b9ba8a1ac37231f6a994a7fa
Jason Kartz <jasonkartz@gmail.com>
2022-09-11 22:31:19 -0500

    attached desktop bg pattern to body

--- pages/index.js
+++ pages/index.js
@@ -10,56 +10,51 @@ export default function Home() {
   return (
     <>
       <Layout />
-      <div className="bg-design">
-        <main>
-          <section className="banner-home">
-            <h1>
-              Award-winning custom designs and digital branding solutions{" "}
-            </h1>
-            <p>
-              With over 10 years in the industry, we are experienced in creating
-              fully responsive websites, app design, and engaging brand
-              experiences. Find out more about our services.
-            </p>
-            <button className="btn-on-dark">Learn more </button>
-          </section>
-          <section className="category-container-home">
-            <CategoryCard background="category-web-home" title="web design" />
-            <CategoryCard background="category-app" title="app design" />
-            <CategoryCard
-              background="category-graphic"
-              title="graphic design"
-            />
-          </section>
-          <section className="infographic-container">
-            <Infographic
-              image={passionateImg}
-              alt="passionate"
-              title="Passionate"
-              description="Each
+
+      <main>
+        <section className="banner-home">
+          <h1>Award-winning custom designs and digital branding solutions </h1>
+          <p>
+            With over 10 years in the industry, we are experienced in creating
+            fully responsive websites, app design, and engaging brand
+            experiences. Find out more about our services.
+          </p>
+          <button className="btn-on-dark">Learn more </button>
+        </section>
+        <section className="category-container-home">
+          <CategoryCard background="category-web-home" title="web design" />
+          <CategoryCard background="category-app" title="app design" />
+          <CategoryCard background="category-graphic" title="graphic design" />
+        </section>
+        <section className="infographic-container">
+          <Infographic
+            image={passionateImg}
+            alt="passionate"
+            title="Passionate"
+            description="Each
         project starts with an in-depth brand research to ensure we only create
         products that serve a purpose. We merge art, design, and technology into
         exciting new solutions."
-            />
-            <Infographic
-              image={resourcefulImg}
-              alt="resourceful"
-              title="Resourceful"
-              description="Everything that we do has a strategic purpose. We use an agile
+          />
+          <Infographic
+            image={resourcefulImg}
+            alt="resourceful"
+            title="Resourceful"
+            description="Everything that we do has a strategic purpose. We use an agile
           approach in all of our projects and value customer collaboration. It
           guarantees superior results that fulfill our clients’ needs."
-            />
-            <Infographic
-              image={friendlyImg}
-              alt="friendly"
-              title="Friendly"
-              description="We are a group of enthusiastic folks who know how to put people
+          />
+          <Infographic
+            image={friendlyImg}
+            alt="friendly"
+            title="Friendly"
+            description="We are a group of enthusiastic folks who know how to put people
           first. Our success depends on our customers, and we strive to give them
           the best experience a company can provide."
-            />
-          </section>
-        </main>
-      </div>
+          />
+        </section>
+      </main>
+
       <Footer includeContactCard={true} />
     </>
   );

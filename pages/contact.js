f63d0aca8bfb73ab8807e36dc5f03b4e2c99fb19
Jason Kartz <jasonkartz@gmail.com>
2022-09-14 21:09:30 -0500

    started contact page html

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,34 @@
 import Footer from "../components/footer";
 import Layout from "../components/layout";
+import LocationDirectory from "../components/LocationDirectory";
 
 export default function Contact() {
   return (
     <>
       <Layout />
-      <main>Contact</main>
+      <main>
+        <section>
+          <div>
+            <h1>Conact Us</h1>
+            <p>
+              Ready to take it to the next level? Let’s talk about your project
+              or idea and find out how we can help your business grow. If you
+              are looking for unique digital experiences that’s relatable to
+              your users, drop us a line.
+            </p>
+          </div>
+          <form>
+            <input type="text" name="name" placeholder="Name" />
+            <input type="email" name="email" placeholder="Email Address" />
+            <input type="tel" name="phone" placeholder="Phone" />
+            <textarea name="message" placeholder="Your Message" />
+            <button type="submit" className="btn-on-dark">
+              Submit
+            </button>
+          </form>
+        </section>
+        <LocationDirectory />
+      </main>
       <Footer includeContactCard={false} />
     </>
   );
